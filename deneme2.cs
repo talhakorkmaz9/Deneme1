9aed391acafbc2c704d7d05c82eae87a5578d6a3
Talha Korkmaz <talhakorkmaz374@gmail.com>
2026-07-26 17:51:10 +0300

    ilk commit

--- deneme2.cs
+++ deneme2.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Merhaba");
+        int a = 5;
+        int b = 10;
+        int toplam = Topla(a, b);
+
+        Console.WriteLine("Sonuç: " + toplam);
+    }
+
+    static int Topla(int x, int y)
+    {
+        int sonuc = x + y;
+        return sonuc;
     }
 }
